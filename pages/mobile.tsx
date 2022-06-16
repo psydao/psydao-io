f77ed1ee611c19ad943c17df0254acea56bd3b02
Raúl R. Pearson <raulrpearson@protonmail.com>
2022-06-16 13:49:06 +0300

    More mobile troubleshooting

--- pages/mobile.tsx
+++ pages/mobile.tsx
@@ -1,4 +1,4 @@
-import { Text } from "@chakra-ui/react";
+import { Box, Text } from "@chakra-ui/react";
 import type { NextPage } from "next";
 import { useLayoutEffect, useState } from "react";
 
@@ -18,10 +18,10 @@ const Mobile: NextPage = () => {
   }, []);
 
   return (
-    <>
+    <Box h="100vh" w="100vw">
       <Text>{height ? `Height: ${height}` : "Not able to measure height"}</Text>
       <Text>{width ? `Width: ${width}` : "Not able to measure width"}</Text>
-    </>
+    </Box>
   );
 };
 
