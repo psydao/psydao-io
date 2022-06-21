30e8eeca2db523b4d5a55a31fcfcfea4fcd46923
Raúl R. Pearson <raulrpearson@protonmail.com>
2022-06-21 16:29:32 +0300

    Clean up window code

--- components/window.tsx
+++ components/window.tsx
@@ -4,7 +4,6 @@ import { isValidMotionProp, motion, useMotionValue } from "framer-motion";
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
-// import { GlobalContext } from "../components/global-context";
 import { useItemContext } from "components/item";
 import { useDesktopContext } from "components/desktop";
 import { CloseIcon, DragIcon, ResizeIcon } from "components/icons";
@@ -16,9 +15,6 @@ const MotionDiv = chakra(motion.div, {
 const clamp = (value: number, min: number, max: number) =>
   Math.max(min, Math.min(value, max));
 
-// TODO this typing is a bit sketchy. It's there so that we can access the prop
-// in the component's body. We don't want to require it because WindowManager
-// will supply it. We can possibly find a cleaner solution.
 interface WindowProps extends React.ComponentPropsWithoutRef<typeof MotionDiv> {
   constraints?: {
     maxHeight?: number;
@@ -27,14 +23,12 @@ interface WindowProps extends React.ComponentPropsWithoutRef<typeof MotionDiv> {
     minWidth?: number;
   };
   contentBoxProps?: BoxProps;
-  focus?(): void;
   initial?: {
     height?: number;
     width?: number;
     x?: number;
     y?: number;
   };
-  onClose?(): void;
   title?: string;
 }
 
@@ -67,18 +61,18 @@ export const Window = ({
       <MotionDiv
         position="absolute"
         zIndex={windowStack && id && windowStack[id]}
-        display="flex"
-        flexDirection="column"
-        border="1px solid #f2bebe"
-        backgroundColor="#faffff"
         onTapStart={() => {
           focus && id && focus(id);
         }}
         style={{ height, x, y, width }}
+        display="flex"
+        flexDirection="column"
+        border="1px solid #f2bebe"
+        backgroundColor="#faffff"
         overflow="hidden"
-        userSelect="none"
         pb="2"
         boxShadow="2xl"
+        userSelect="none"
         pointerEvents="auto"
         {...rest}
       >
@@ -89,15 +83,16 @@ export const Window = ({
           p="2"
           gap="2"
         >
-          <motion.div
+          <MotionDiv
+            width="2em"
+            sx={{ touchAction: "none" }}
             onPan={(e, info) => {
               x.set(info.delta.x + x.get());
               y.set(info.delta.y + y.get());
             }}
-            style={{ width: "2em", touchAction: "none" }}
           >
             <DragIcon />
-          </motion.div>
+          </MotionDiv>
           {title}
           <CloseIcon flex="0 0 auto" onClick={onClose} />
         </Box>
@@ -110,7 +105,8 @@ export const Window = ({
         >
           {children}
         </Box>
-        <motion.div
+        <MotionDiv
+          sx={{ touchAction: "none" }}
           onPan={(e, info) => {
             const newHeight = info.delta.y + height.get();
             const newWidth = info.delta.x + width.get();
@@ -121,10 +117,9 @@ export const Window = ({
               width.set(newWidth);
             }
           }}
-          style={{ touchAction: "none" }}
         >
           <ResizeIcon position="absolute" right="2" bottom="2" />
-        </motion.div>
+        </MotionDiv>
       </MotionDiv>,
       windowLayerRef.current
     );
