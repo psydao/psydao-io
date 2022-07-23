3fa18b8c7c760a197935cdde5344431121298c48
Raúl R. Pearson <raulrpearson@protonmail.com>
2022-07-23 17:08:27 +0300

    Add TODO comment

--- components/motion-box.tsx
+++ components/motion-box.tsx
@@ -1,6 +1,7 @@
 import { chakra } from "@chakra-ui/react";
 import { isValidMotionProp, motion } from "framer-motion";
 
+// TODO improve types, I'm not getting auto-completion for props
 export const MotionBox = chakra(motion.div, {
   shouldForwardProp: (prop) => isValidMotionProp(prop) || prop === "children",
 });
