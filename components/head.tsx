eaa2102f3380acb498c207b7f0cfe1c50fdf7747
Raúl R. Pearson <raulrpearson@protonmail.com>
2022-08-03 15:22:30 +0300

    Improve head metas

--- components/head.tsx
+++ components/head.tsx
@@ -1,19 +1,27 @@
 import NextHead from "next/head";
 
-export const Head = () => {
+interface HeadProps {
+  title?: string;
+  image?: string;
+  children?: React.ReactNode;
+}
+
+export const Head = ({
+  title = "PsyDAO - A Psychedelics Research DAO",
+  image = "/psydao-seo-image.jpg",
+  children = null,
+}: HeadProps) => {
   return (
     <NextHead>
-      <title>PsyDAO - A Psychedelics Research DAO</title>
+      <title>{title}</title>
+      <meta content={title} property="og:title" />
+      <meta content={title} property="twitter:title" />
       <meta
-        content="PsyDAO - A Psychedelics Research DAO"
-        property="og:title"
+        name="description"
+        content="PsyDAO is forming a decentralised organisation with the goal of funding research at the intersection of psychedelics and mental health."
       />
-      <meta content="/psydao-seo-image.jpg" property="og:image" />
-      <meta
-        content="PsyDAO - A Psychedelics Research DAO"
-        property="twitter:title"
-      />
-      <meta content="/psydao-seo-image.jpg" property="twitter:image" />
+      <meta content={image} property="og:image" />
+      <meta content={image} property="twitter:image" />
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <link
         href="/psydao-favicon-black.png"
@@ -21,6 +29,7 @@ export const Head = () => {
         type="image/x-icon"
       />
       <link href="/psydao-apple-touch.png" rel="apple-touch-icon" />
+      {children}
     </NextHead>
   );
 };
