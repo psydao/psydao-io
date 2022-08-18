721caf259ef2decd8feb02388cec90e695b9e878
Raúl R. Pearson <raulrpearson@protonmail.com>
2022-08-18 12:42:52 +0300

    Iterate Get Involved design

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,11 +7,11 @@ import { BackgroundGrid } from "components/background-grid";
 import { Blobs } from "components/blobs";
 import { Grid } from "components/grid";
 import { Head } from "components/head";
+import { Logo } from "components/icons";
 import { Item } from "components/item";
 import { Manifesto } from "components/manifesto";
 import { Marquee } from "components/marquee";
 import { Newsletter } from "components/newsletter";
-import { Pill } from "components/pill";
 import { Youtube } from "components/video";
 import { WindowManager } from "components/window-manager";
 import { psydaoDescription } from "lib/constants";
@@ -56,7 +56,7 @@ const Homepage: NextPage = () => {
             <Grid
               position="relative"
               zIndex="0"
-              getNumberOfFillers={(cols, rows) => cols * (rows - 1) - 6}
+              getNumberOfFillers={(cols, rows) => cols * (rows - 1) - 7}
             >
               <Box gridArea="1 / 1 / 3 / 3">
                 <Image src="/psydao-deep-logo.svg" alt="" h="100%" w="100%" />
@@ -69,14 +69,14 @@ const Homepage: NextPage = () => {
                   height="calc(100% - 2px)"
                   width="100%"
                   gridArea={{
-                    base: "1 / 1 / span 5 / -1",
+                    base: "1 / 1 / span 4 / -1",
                     lg: "1 / 1 / span 1 / -1",
                   }}
                   pos="absolute"
                   border="none !important"
                   display="grid"
-                  gridTemplateColumns={{ base: "1fr", lg: "repeat(5, auto)" }}
-                  gridTemplateRows={{ base: "repeat(5, 1fr)", lg: "1fr" }}
+                  gridTemplateColumns={{ base: "1fr", lg: "repeat(4, auto)" }}
+                  gridTemplateRows={{ base: "repeat(4, 1fr)", lg: "1fr" }}
                   alignItems="center"
                   justifyItems="end"
                   justifyContent="end"
@@ -86,7 +86,7 @@ const Homepage: NextPage = () => {
                   <Youtube />
                   <Manifesto />
                   <Newsletter />
-                  <Item id="discord">
+                  <Item id="get-involved">
                     <Item.Icon
                       px="8"
                       borderRadius="full"
@@ -106,31 +106,35 @@ const Homepage: NextPage = () => {
                       }}
                     >
                       <Link
-                        href="https://discord.gg/hUH4MWxVFx"
+                        href="https://airtable.com/shrCaOD9DaD57J3Mu"
                         target="_blank"
                         display="flex"
                         alignItems="center"
                         gap="2"
                         _hover={{ textDecor: "none" }}
                       >
-                        <Icon as={FaDiscord} />
-                        Join Us
-                      </Link>
-                    </Item.Icon>
-                  </Item>
-                  <Item id="get-involved">
-                    <Item.Icon>
-                      <Link
-                        href="https://airtable.com/shrCaOD9DaD57J3Mu"
-                        target="_blank"
-                        _hover={{ textDecor: "none" }}
-                      >
-                        <Pill>Get Involved</Pill>
+                        <Logo />
+                        Get Involved
                       </Link>
                     </Item.Icon>
                   </Item>
                 </Box>
               </WindowManager>
+              <Link
+                href="https://discord.gg/hUH4MWxVFx"
+                target="_blank"
+                gridArea="-3 / -4 / span 1 / span 1"
+                p="30%"
+                color="#f2bebe"
+                _hover={{
+                  color: "#E69CFF",
+                  backgroundImage:
+                    "linear-gradient(to bottom, #ffffff 0%, #f3ffe9 50.52%, #e7feff 100%)",
+                }}
+                transition="all 200ms ease"
+              >
+                <Icon as={FaDiscord} boxSize="full" />
+              </Link>
               <Link
                 href="https://twitter.com/psy_dao"
                 target="_blank"
