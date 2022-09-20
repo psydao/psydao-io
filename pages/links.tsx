047e73254089e7612a2d74f83f130dd05ace59e1
Raúl R. Pearson <raulrpearson@protonmail.com>
2022-09-20 16:55:10 +0200

    Modify links page design

--- pages/links.tsx
+++ pages/links.tsx
@@ -1,107 +1,174 @@
 import {
+  Box,
   Container,
-  Divider,
   Heading,
   HStack,
   Icon,
-  Link,
   VStack,
 } from "@chakra-ui/react";
+import { motion } from "framer-motion";
+import Image from "next/future/image";
 import { FaDiscord, FaTwitter, FaYoutube } from "react-icons/fa";
 import { RiInstagramFill } from "react-icons/ri";
 
-import { Pill } from "components/pill";
-// TODO fix missing links
-import { LogoStacked } from "components/icons";
+import { MotionBox } from "components/motion-box";
 import linksPageData from "data/links-page-data.json";
+import backgroundImage from "../public/links/background.png";
+import logo from "../public/links/wordmark-purple-h.svg";
+
+interface LinkSpec {
+  color?: string;
+  href: string;
+  label: string;
+  variant?: string;
+}
+
+const LinkPill = ({ color = "#9835BA", label, href, variant }: LinkSpec) => {
+  let variantStyles = {};
+
+  switch (variant) {
+    case "special-fill":
+      variantStyles = {
+        background:
+          "center / cover no-repeat url(/links/stars.png), linear-gradient(268.15deg, #B7FFFF 11.65%, #E69CFF 105.11%)",
+        backgroundBlendMode: "screen, normal",
+      };
+      break;
+  }
+
+  return (
+    <a href={href} target="_blank" rel="noreferrer">
+      <MotionBox
+        border={`1px solid ${color}`}
+        color={color}
+        borderRadius="full"
+        fontStyle="italic"
+        fontSize="24px"
+        lineHeight="36px"
+        p="7px"
+        textAlign="center"
+        whileHover={{ scale: 1.02 }}
+        whileTap={{ scale: 0.98 }}
+        {...variantStyles}
+      >
+        {label}
+      </MotionBox>
+    </a>
+  );
+};
 
 const LinksPage = () => {
   const { subtitle, groups } = linksPageData;
   return (
-    <Container>
-      <LogoStacked
-        width="150px"
-        display="block"
-        mx="auto"
-        my="8"
-        color="#f2bebe"
+    <Box
+      position="absolute"
+      top="0"
+      left="0"
+      h="100vh"
+      w="100vw"
+      px={["2", "4"]}
+    >
+      <Image
+        src={backgroundImage}
+        alt=""
+        style={{
+          position: "fixed",
+          top: "0",
+          left: "0",
+          zIndex: -1,
+          height: "100%",
+          width: "100%",
+          objectFit: "cover",
+          objectPosition: "center",
+        }}
       />
-      <Heading as="h2" color="#f2bebe" fontStyle="italic" textAlign="center">
-        {subtitle}
-      </Heading>
-      <Divider border="2px" my="8" borderColor="#f2bebe" />
-      <VStack align="stretch" gap="8">
-        {groups.map(({ heading, links }) => {
-          return (
-            <VStack key={heading} align="stretch" gap="4">
-              <Heading as="h3" color="#f2bebe" fontStyle="italic">
-                {heading}
-              </Heading>
-              {links.map(({ label, href }) => (
-                <a
-                  key={label + href}
-                  href={href}
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <Pill>{label}</Pill>
-                </a>
-              ))}
-            </VStack>
-          );
-        })}
-      </VStack>
-      <HStack justify="center" my="16" gap="4">
-        <Link
-          href="https://twitter.com/psy_dao"
-          target="_blank"
-          color="#f2bebe"
-          lineHeight="0"
-          _hover={{
-            color: "#a4ffff",
-          }}
-          transition="all 200ms ease"
-        >
-          <Icon as={FaTwitter} boxSize="10" />
-        </Link>
-        <Link
-          href="https://discord.gg/hUH4MWxVFx"
-          target="_blank"
-          color="#f2bebe"
-          lineHeight="0"
-          _hover={{
-            color: "#E69CFF",
-          }}
-          transition="all 200ms ease"
-        >
-          <Icon as={FaDiscord} boxSize="10" />
-        </Link>
-        <Link
-          href="https://www.youtube.com/channel/UC8bjrtWOPuHdvMfZ3ScAI-Q"
-          target="_blank"
-          color="#f2bebe"
-          lineHeight="0"
-          _hover={{
-            color: "#dc4e4e",
-          }}
-          transition="all 200ms ease"
-        >
-          <Icon as={FaYoutube} boxSize="10" />
-        </Link>
-        <Link
-          href="https://www.instagram.com/psyDAO_"
-          target="_blank"
-          color="#f2bebe"
-          lineHeight="0"
-          _hover={{
-            color: "#405de6",
-          }}
-          transition="all 200ms ease"
-        >
-          <Icon as={RiInstagramFill} boxSize="10" />
-        </Link>
-      </HStack>
-    </Container>
+      <Container
+        maxW="590px"
+        bg="#fffafa"
+        pt="9"
+        px={["4", "8"]}
+        pb="14"
+        my={["2", "4"]}
+        borderRadius="lg"
+      >
+        <Image
+          src={logo}
+          alt=""
+          style={{ maxWidth: 257, margin: "0 auto 26px" }}
+        />
+        <Heading as="h2" fontStyle="italic" textAlign="center" fontSize="26px">
+          {subtitle}
+        </Heading>
+        <VStack align="stretch" gap="8" mt="8">
+          {groups.map(({ heading, links, color = "#9835BA" }) => {
+            return (
+              <VStack
+                key={heading + JSON.stringify(links)}
+                align="stretch"
+                gap="3"
+              >
+                {heading && (
+                  <Heading
+                    as="h3"
+                    fontStyle="italic"
+                    fontSize="24px"
+                    textAlign="center"
+                    color={color}
+                  >
+                    {heading}
+                  </Heading>
+                )}
+                {links.map((linkProps) => (
+                  <LinkPill
+                    key={JSON.stringify(linkProps)}
+                    color={color}
+                    {...linkProps}
+                  />
+                ))}
+              </VStack>
+            );
+          })}
+        </VStack>
+        <HStack justify="center" mt="14" gap="4" color="#E69CFF">
+          <motion.a
+            href="https://twitter.com/psy_dao"
+            target="_blank"
+            whileHover={{ scale: 1.05 }}
+            whileTap={{ scale: 0.95 }}
+            style={{ lineHeight: 0 }}
+          >
+            <Icon as={FaTwitter} boxSize="9" />
+          </motion.a>
+          <motion.a
+            href="https://www.youtube.com/channel/UC8bjrtWOPuHdvMfZ3ScAI-Q"
+            target="_blank"
+            whileHover={{ scale: 1.05 }}
+            whileTap={{ scale: 0.95 }}
+            style={{ lineHeight: 0 }}
+          >
+            <Icon as={FaYoutube} boxSize="9" />
+          </motion.a>
+          <motion.a
+            href="https://discord.gg/hUH4MWxVFx"
+            target="_blank"
+            whileHover={{ scale: 1.05 }}
+            whileTap={{ scale: 0.95 }}
+            style={{ lineHeight: 0 }}
+          >
+            <Icon as={FaDiscord} boxSize="9" />
+          </motion.a>
+          <motion.a
+            href="https://www.instagram.com/psyDAO_"
+            target="_blank"
+            whileHover={{ scale: 1.05 }}
+            whileTap={{ scale: 0.95 }}
+            style={{ lineHeight: 0 }}
+          >
+            <Icon as={RiInstagramFill} boxSize="9" />
+          </motion.a>
+        </HStack>
+      </Container>
+    </Box>
   );
 };
 
