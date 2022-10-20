805c106a3c029ba14c1237a9882791ad9098b0be
Raúl R. Pearson <raulrpearson@protonmail.com>
2022-10-20 17:54:44 +0300

    Add Head metas to links page

--- pages/links.tsx
+++ pages/links.tsx
@@ -12,6 +12,7 @@ import Image from "next/future/image";
 import { FaDiscord, FaTwitter, FaYoutube } from "react-icons/fa";
 import { RiInstagramFill } from "react-icons/ri";
 
+import { Head } from "components/head";
 import { MotionBox } from "components/motion-box";
 import linksPageData from "data/links-page-data.json";
 import backgroundImage from "../public/links/background.png";
@@ -61,128 +62,138 @@ const LinkPill = ({ color = "#9835BA", label, href, variant }: LinkSpec) => {
 const LinksPage = () => {
   const { subtitle, groups } = linksPageData;
   return (
-    <Center
-      position="absolute"
-      top="0"
-      left="0"
-      minH="100vh"
-      w="100vw"
-      px={["2", "4"]}
-    >
-      <Image
-        src={backgroundImage}
-        alt=""
-        style={{
-          position: "fixed",
-          top: "0",
-          left: "0",
-          zIndex: -1,
-          height: "100%",
-          width: "100%",
-          objectFit: "cover",
-          objectPosition: "center",
-        }}
-      />
-      <Box
-        maxW="450px"
-        bg="#fffafa"
-        pt="9"
-        px={["4", "8"]}
-        pb="14"
-        my={["2", "4"]}
-        borderRadius="lg"
+    <>
+      <Head />
+      <Center
+        position="absolute"
+        top="0"
+        left="0"
+        minH="100vh"
+        w="100vw"
+        px={["2", "4"]}
       >
         <Image
-          src={logo}
+          src={backgroundImage}
           alt=""
-          style={{ maxWidth: 257, margin: "0 auto 26px" }}
+          style={{
+            position: "fixed",
+            top: "0",
+            left: "0",
+            zIndex: -1,
+            height: "100%",
+            width: "100%",
+            objectFit: "cover",
+            objectPosition: "center",
+          }}
         />
-        <Heading as="h2" fontStyle="italic" textAlign="center" fontSize="26px">
-          {subtitle}
-        </Heading>
-        <VStack align="stretch" gap="8" mt="8">
-          {groups.map(({ heading, description, links, color = "#9835BA" }) => {
-            return (
-              <VStack
-                key={heading + JSON.stringify(links)}
-                align="stretch"
-                gap="3"
-              >
-                <div>
-                  {heading && (
-                    <Heading
-                      as="h3"
-                      fontStyle="italic"
-                      fontSize="24px"
-                      textAlign="center"
-                      color={color}
-                    >
-                      {heading}
-                    </Heading>
-                  )}
-                  {description && (
-                    <Text
-                      as="p"
-                      fontStyle="italic"
-                      fontSize="18px"
-                      textAlign="center"
-                      color={color}
-                    >
-                      {description}
-                    </Text>
-                  )}
-                </div>
-                {links.map((linkProps) => (
-                  <LinkPill
-                    key={JSON.stringify(linkProps)}
-                    color={color}
-                    {...linkProps}
-                  />
-                ))}
-              </VStack>
-            );
-          })}
-        </VStack>
-        <HStack justify="center" mt="14" gap="4" color="#E69CFF">
-          <motion.a
-            href="https://twitter.com/psy_dao"
-            target="_blank"
-            whileHover={{ scale: 1.05 }}
-            whileTap={{ scale: 0.95 }}
-            style={{ lineHeight: 0 }}
+        <Box
+          maxW="450px"
+          bg="#fffafa"
+          pt="9"
+          px={["4", "8"]}
+          pb="14"
+          my={["2", "4"]}
+          borderRadius="lg"
+        >
+          <Image
+            src={logo}
+            alt=""
+            style={{ maxWidth: 257, margin: "0 auto 26px" }}
+          />
+          <Heading
+            as="h2"
+            fontStyle="italic"
+            textAlign="center"
+            fontSize="26px"
           >
-            <Icon as={FaTwitter} boxSize="9" />
-          </motion.a>
-          <motion.a
-            href="https://www.youtube.com/channel/UC8bjrtWOPuHdvMfZ3ScAI-Q"
-            target="_blank"
-            whileHover={{ scale: 1.05 }}
-            whileTap={{ scale: 0.95 }}
-            style={{ lineHeight: 0 }}
-          >
-            <Icon as={FaYoutube} boxSize="9" />
-          </motion.a>
-          <motion.a
-            href="https://discord.gg/hUH4MWxVFx"
-            target="_blank"
-            whileHover={{ scale: 1.05 }}
-            whileTap={{ scale: 0.95 }}
-            style={{ lineHeight: 0 }}
-          >
-            <Icon as={FaDiscord} boxSize="9" />
-          </motion.a>
-          <motion.a
-            href="https://www.instagram.com/psyDAO_"
-            target="_blank"
-            whileHover={{ scale: 1.05 }}
-            whileTap={{ scale: 0.95 }}
-            style={{ lineHeight: 0 }}
-          >
-            <Icon as={RiInstagramFill} boxSize="9" />
-          </motion.a>
-        </HStack>
-      </Box>
-    </Center>
+            {subtitle}
+          </Heading>
+          <VStack align="stretch" gap="8" mt="8">
+            {groups.map(
+              ({ heading, description, links, color = "#9835BA" }) => {
+                return (
+                  <VStack
+                    key={heading + JSON.stringify(links)}
+                    align="stretch"
+                    gap="3"
+                  >
+                    <div>
+                      {heading && (
+                        <Heading
+                          as="h3"
+                          fontStyle="italic"
+                          fontSize="24px"
+                          textAlign="center"
+                          color={color}
+                        >
+                          {heading}
+                        </Heading>
+                      )}
+                      {description && (
+                        <Text
+                          as="p"
+                          fontStyle="italic"
+                          fontSize="18px"
+                          textAlign="center"
+                          color={color}
+                        >
+                          {description}
+                        </Text>
+                      )}
+                    </div>
+                    {links.map((linkProps) => (
+                      <LinkPill
+                        key={JSON.stringify(linkProps)}
+                        color={color}
+                        {...linkProps}
+                      />
+                    ))}
+                  </VStack>
+                );
+              }
+            )}
+          </VStack>
+          <HStack justify="center" mt="14" gap="4" color="#E69CFF">
+            <motion.a
+              href="https://twitter.com/psy_dao"
+              target="_blank"
+              whileHover={{ scale: 1.05 }}
+              whileTap={{ scale: 0.95 }}
+              style={{ lineHeight: 0 }}
+            >
+              <Icon as={FaTwitter} boxSize="9" />
+            </motion.a>
+            <motion.a
+              href="https://www.youtube.com/channel/UC8bjrtWOPuHdvMfZ3ScAI-Q"
+              target="_blank"
+              whileHover={{ scale: 1.05 }}
+              whileTap={{ scale: 0.95 }}
+              style={{ lineHeight: 0 }}
+            >
+              <Icon as={FaYoutube} boxSize="9" />
+            </motion.a>
+            <motion.a
+              href="https://discord.gg/hUH4MWxVFx"
+              target="_blank"
+              whileHover={{ scale: 1.05 }}
+              whileTap={{ scale: 0.95 }}
+              style={{ lineHeight: 0 }}
+            >
+              <Icon as={FaDiscord} boxSize="9" />
+            </motion.a>
+            <motion.a
+              href="https://www.instagram.com/psyDAO_"
+              target="_blank"
+              whileHover={{ scale: 1.05 }}
+              whileTap={{ scale: 0.95 }}
+              style={{ lineHeight: 0 }}
+            >
+              <Icon as={RiInstagramFill} boxSize="9" />
+            </motion.a>
+          </HStack>
+        </Box>
+      </Center>
+    </>
   );
 };
 
