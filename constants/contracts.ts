b5e45733d96e3ed18b6354beb8a7a8e433f57cf8
arlindlinum <arlind@linumlabs.com>
2024-04-23 10:48:57 +0200

    fix: add sepolia

--- constants/contracts.ts
+++ constants/contracts.ts
@@ -1,6 +1,6 @@
 export const tokenSaleContract = "0x8F70d678c370B9B7b5b2BbD8399426b458002A9F"; // MAINNET
 export const tokenSaleContractSepolia =
-  "0x3C6bF5a124772926CC80fA2ED6f01C1ee596Ee6f"; // SEPOLIA TESTNET
+  "0x6c6Ab7b3215374dE4A65De63eAC9BC7A0c7f402d"; // SEPOLIA
 
 // Chainlink
 export const chainLinkMainnet = "0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419";
