88cd7518e5ec463cf510cca63ad1bb6b8d20b9b6
dev_waliba <donwaleyb@gmail.com>
2024-06-28 17:41:27 +0100

    compose mint components into mint section

---
+++ components/commons/nftprice.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Image, Flex, Text } from "@chakra-ui/react";
+
+interface NFTPriceProps {
+  price: string;
+}
+
+const NFTPrice = ({ price }: NFTPriceProps) => {
+  return (
+    <Flex
+      alignItems="center"
+      position="absolute"
+      bottom="10px"
+      left="10px"
+      bg="white"
+      p={2}
+      borderRadius="10px"
+      fontWeight="bold"
+    >
+      <Text>{price}</Text>
+      <Image src="/windows/swap/ETH.svg" alt="ETH Symbol" ml={1} />
+    </Flex>
+  );
+};
+
+export default NFTPrice;
