dbc38b89221ccf3267b1cef1ee6d744c3933fd0b
Ester <ester@linumlabs.com>
2024-07-11 11:46:01 +0200

    feat: update whitelist functionality

---
+++ components/admin-dashboard/whitelisted-addresses.ts
@@ -0,0 +1,5 @@
+// Feel free to add your own, this is just dummy data :)
+export const whitelistedAddresses = [
+  "0x8754a4c886f8Cb77a1d2F38470c653DDb4727f21",
+  "0xD9c0BB3476CE2AD2102D3AC07287BB802EeA98f1"
+];
