ebcd8ce2a83173b9ef2657ccc00932a8dec64685
dev_waliba <donwaleyb@gmail.com>
2024-07-17 13:32:44 +0100

    update whitelist

--- components/admin-dashboard/whitelisted-addresses.ts
+++ components/admin-dashboard/whitelisted-addresses.ts
@@ -1,5 +1,6 @@
 // Feel free to add your own, this is just dummy data :)
 export const whitelistedAddresses = [
   "0x8754a4c886f8Cb77a1d2F38470c653DDb4727f21",
-  "0xD9c0BB3476CE2AD2102D3AC07287BB802EeA98f1"
+  "0xD9c0BB3476CE2AD2102D3AC07287BB802EeA98f1",
+  "0xF6CDB1E733EA00D0eEa1A32F218B0ec76ABF1517"
 ];
