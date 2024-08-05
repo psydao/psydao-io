595acfa9cd7e41e79ba3531c2d72a3f5f4faff47
dev_waliba <donwaleyb@gmail.com>
2024-08-05 14:46:10 +0100

    keep sale widget context on ice

--- providers/SaleWidgetContext.tsx
+++ providers/SaleWidgetContext.tsx
@@ -1,38 +1,38 @@
-import React, {
-  createContext,
-  useState,
-  type ReactNode,
-  useContext
-} from "react";
+// import React, {
+//   createContext,
+//   useState,
+//   type ReactNode,
+//   useContext
+// } from "react";
 
-interface SaleWidgetContextType {}
+// interface SaleWidgetContextType {}
 
-const SaleWidgetContext = createContext<SaleWidgetContextType | undefined>(
-  undefined
-);
+// const SaleWidgetContext = createContext<SaleWidgetContextType | undefined>(
+//   undefined
+// );
 
-interface SaleWidgetProviderProps {
-  children: ReactNode;
-}
+// interface SaleWidgetProviderProps {
+//   children: ReactNode;
+// }
 
-const SaleWidgetProvider: React.FC<SaleWidgetProviderProps> = ({
-  children
-}) => {
-  return (
-    <SaleWidgetContext.Provider value={{}}>
-      {children}
-    </SaleWidgetContext.Provider>
-  );
-};
+// const SaleWidgetProvider: React.FC<SaleWidgetProviderProps> = ({
+//   children
+// }) => {
+//   return (
+//     <SaleWidgetContext.Provider value={{}}>
+//       {children}
+//     </SaleWidgetContext.Provider>
+//   );
+// };
 
-export const useSaleWidgetContext = (): SaleWidgetContextType => {
-  const context = useContext(SaleWidgetContext);
-  if (!context) {
-    throw new Error(
-      "useSaleWidgetContext must be used within a SaleWidgetProvider"
-    );
-  }
-  return context;
-};
+// export const useSaleWidgetContext = (): SaleWidgetContextType => {
+//   const context = useContext(SaleWidgetContext);
+//   if (!context) {
+//     throw new Error(
+//       "useSaleWidgetContext must be used within a SaleWidgetProvider"
+//     );
+//   }
+//   return context;
+// };
 
-export default SaleWidgetProvider;
+// export default SaleWidgetProvider;
