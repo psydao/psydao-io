eb5f6ad3467ff3590014b70a4e6f32e210894b62
Bevan Christians <gaelicgod@gmail.com>
2024-08-29 00:05:06 +0200

    chore: add new packages
    feat: add shopify discount endpoints
    
    feat: add demo product token gate
    
    chore: add new env vars
    
    refactor: remove secrets

---
+++ pages/api/shopify.ts
@@ -0,0 +1,254 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import "@shopify/shopify-api/adapters/node";
+import { shopifyApi, LATEST_API_VERSION } from "@shopify/shopify-api";
+import { type Address, getAddress, createPublicClient, http } from "viem";
+import { mainnet } from "viem/chains";
+
+interface ShopifyResponse {
+  discountCodeBasicCreate: {
+    codeDiscountNode: {
+      codeDiscount: {
+        codes: {
+          edges: Array<{
+            node: {
+              code: string;
+            };
+          }>;
+        };
+      };
+    };
+    userErrors: Array<{ field: string; message: string }>;
+  };
+}
+
+const MAINNET_NFT_CONTRACT_ADDRESS =
+  process.env.NEXT_PUBLIC_MAINNET_ERC721_CONTRACT_ADDRESS ?? "";
+const SEPOLIA_NFT_CONTRACT_ADDRESS =
+  process.env.NEXT_PUBLIC_SEPOLIA_ERC721_CONTRACT_ADDRESS ?? "";
+
+const CHAIN_ID = process.env.NEXT_PUBLIC_CHAIN_ID ?? "1";
+
+const SHOPIFY_API_ACCESS_TOKEN = process.env.SHOPIFY_API_ACCESS_TOKEN ?? "";
+const SHOPIFY_API_KEY = process.env.SHOPIFY_API_KEY ?? "";
+const SHOPIFY_API_SECRET = process.env.SHOPIFY_API_SECRET ?? "";
+const SHOPIFY_SHOP_NAME = process.env.SHOPIFY_SHOP_NAME ?? "";
+
+const NFT_CONTRACT_ADDRESS =
+  CHAIN_ID === "1"
+    ? MAINNET_NFT_CONTRACT_ADDRESS
+    : SEPOLIA_NFT_CONTRACT_ADDRESS;
+const ETHEREUM_RPC_URL = process.env.NEXT_PUBLIC_MAINNET_CLIENT ?? "";
+
+const publicClient = createPublicClient({
+  chain: mainnet,
+  transport: http(ETHEREUM_RPC_URL)
+});
+
+console.debug("SHOPIFY_API_ACCESS_TOKEN => ", SHOPIFY_API_ACCESS_TOKEN);
+
+const shopifyClient = shopifyApi({
+  apiKey: SHOPIFY_API_KEY,
+  apiSecretKey: SHOPIFY_API_SECRET,
+  adminApiAccessToken: SHOPIFY_API_ACCESS_TOKEN,
+  scopes: ["read_products", "read_discounts", "write_discounts"],
+  hostName: SHOPIFY_SHOP_NAME,
+  apiVersion: LATEST_API_VERSION,
+  isEmbeddedApp: false,
+  isTesting: true
+});
+
+/**
+ * Validates that the user has a valid NFT
+ * @param ethAddress The user's Ethereum address
+ * @returns true if the user has a valid NFT, false otherwise
+ * @throws Error if the validation fails
+ */
+async function validateNFT(ethAddress: Address): Promise<boolean> {
+  try {
+    const nftBalance: bigint = await publicClient.readContract({
+      address: NFT_CONTRACT_ADDRESS as Address,
+      abi: [
+        {
+          name: "balanceOf",
+          type: "function",
+          inputs: [{ name: "owner", type: "address" }],
+          outputs: [{ name: "", type: "uint256" }],
+          stateMutability: "view"
+        }
+      ],
+      functionName: "balanceOf",
+      args: [ethAddress]
+    });
+
+    if (nftBalance === 0n) {
+      return false;
+    }
+
+    return true;
+  } catch (error) {
+    console.error("Error validating NFT:", error);
+    throw new Error("Failed to validate NFT");
+  }
+}
+
+async function generateShopifyProductDiscount(
+  ethAddress: Address
+): Promise<string> {
+  // generate a discount code for 100% off here
+  const session = shopifyClient.session.customAppSession(SHOPIFY_SHOP_NAME);
+  session.accessToken = SHOPIFY_API_ACCESS_TOKEN;
+  console.log("Session details -> ", {
+    shop: session.shop,
+    isActive: session.isActive([
+      "read_products",
+      "read_discounts",
+      "write_discounts"
+    ]),
+    accessToken: session.accessToken ? "Set" : "Not set",
+    scopes: session.scope,
+    configScope: shopifyClient.config.scopes,
+    configAPIKeySet: shopifyClient.config.adminApiAccessToken
+      ? "Set"
+      : "Not set"
+  });
+  const client = new shopifyClient.clients.Graphql({
+    session,
+    apiVersion: LATEST_API_VERSION
+  });
+
+  const discountCode = `PSYDAO-${ethAddress.slice(2, 8)}-${Date.now()}`;
+
+  const mutation = `
+      mutation discountCodeBasicCreate($basicCodeDiscount: DiscountCodeBasicInput!) {
+        discountCodeBasicCreate(basicCodeDiscount: $basicCodeDiscount) {
+          codeDiscountNode {
+            codeDiscount {
+              ... on DiscountCodeBasic {
+                codes(first: 1) {
+                  edges {
+                    node {
+                      code
+                    }
+                  }
+                }
+              }
+            }
+          }
+          userErrors {
+            field
+            message
+          }
+        }
+      }
+    `;
+
+  const variables = {
+    basicCodeDiscount: {
+      title: `PsyDAO Como Hat Discount for ${ethAddress}`,
+      code: discountCode,
+      metafields: [
+        {
+          key: "wallet_address",
+          value: ethAddress,
+          namespace: "psydao"
+        }
+      ],
+      startsAt: new Date().toISOString(),
+      customerSelection: {
+        all: true
+      },
+      usageLimit: 1,
+      appliesOncePerCustomer: true,
+      customerGets: {
+        value: {
+          percentage: 1.0
+        },
+        items: {
+          products: {
+            productsToAdd: ["gid://shopify/Product/8984181407961"] // Replace with actual Como Hat product ID
+          }
+        }
+      }
+    }
+  };
+
+  try {
+    // Note: OLD WAY (deprecated)
+    // const response = await client.query({
+    //   data: { query: mutation, variables }
+    // });
+
+    // Note: NEW WAY
+    const response = await client.request(mutation, {
+      variables
+    });
+
+    if (response.data) {
+      const result = response.data as ShopifyResponse;
+      if (result.discountCodeBasicCreate?.userErrors?.length > 0) {
+        const errorMessage =
+          result.discountCodeBasicCreate.userErrors[0]?.message ??
+          "Unknown error";
+        throw new Error(errorMessage);
+      }
+
+      return discountCode;
+    }
+    throw new Error("Failed to generate discount code");
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error("Error details:", {
+        message: error.message,
+        stack: error.stack,
+        shopName: SHOPIFY_SHOP_NAME,
+        hasAccessToken: !!SHOPIFY_API_ACCESS_TOKEN
+      });
+    } else {
+      console.error("Error details:", {
+        message: error,
+        shopName: SHOPIFY_SHOP_NAME,
+        hasAccessToken: !!SHOPIFY_API_ACCESS_TOKEN
+      });
+    }
+    throw new Error("Failed to generate discount code");
+  }
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method !== "GET") {
+    res.setHeader("Allow", ["GET"]);
+    res.status(405).end(`Method ${req.method} not allowed`);
+    return;
+  }
+
+  try {
+    const ethAddress = req.query.ethAddress as Address;
+    if (!ethAddress || !getAddress(ethAddress)) {
+      return res.status(400).json({
+        message: "ethAddress is required"
+      });
+    }
+
+    const hasNFT = await validateNFT(ethAddress);
+    if (!hasNFT) {
+      return res.status(403).json({
+        message: "User does not have a valid NFT"
+      });
+    }
+
+    const discountCode = await generateShopifyProductDiscount(ethAddress);
+
+    return res.status(200).json({
+      discountCode: discountCode
+    });
+  } catch (error) {
+    console.error("Error generating discount code:", error);
+    return res.status(400).json({
+      message: "An error has occurred",
+      error: error instanceof Error ? error.message : String(error)
+    });
+  }
+}
