0798bd985288b0bb94f5ba9e5a1da3558c63a655
Ester <ester@linumlabs.com>
2024-09-05 10:17:17 +0200

    update address types

--- lib/erc1155-contract-config.ts
+++ lib/erc1155-contract-config.ts
@@ -2,10 +2,11 @@ import { env } from "@/config/env.mjs";
 import ERC1155Abi from "../abis/ERC1155Abi.json";
 import ERC1155AbiSepolia from "../abis/ERC115AbiSepolia.json";
 import { ERC1155Mainnet, ERC1155Sepolia } from "../constants/contracts";
+import type { Address } from "viem";
 const erc1155sContractConfig = {
   address: env.NEXT_PUBLIC_IS_MAINNET
-    ? (ERC1155Mainnet as `0x${string}`)
-    : (ERC1155Sepolia as `0x${string}`),
+    ? (ERC1155Mainnet as Address)
+    : (ERC1155Sepolia as Address),
   abi: env.NEXT_PUBLIC_IS_MAINNET ? ERC1155Abi : ERC1155AbiSepolia
 };
 
