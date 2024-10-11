3d9aef33ba42b71bd9e0e131cb95b3705dfba1c5
Ester <ester@linumlabs.com>
2024-10-11 08:51:11 +0200

    fix: small adjustments

--- pages/api/shopify.ts
+++ pages/api/shopify.ts
@@ -1,7 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import "@shopify/shopify-api/adapters/node";
 import { type Address, getAddress } from "viem";
-import getPOAPStatus from "@/utils/getPOAPStatus";
 import { generateShopifyProductDiscount } from "@/utils/generateProductDiscount";
 import { createCart } from "@/utils/createShopifyCart";
 
@@ -11,8 +10,7 @@ export default async function handler(
 ) {
   if (req.method !== "GET") {
     res.setHeader("Allow", ["GET"]);
-    res.status(405).end(`Method ${req.method} not allowed`);
-    return;
+    return res.status(405).end(`Method ${req.method} not allowed`);
   }
 
   try {
@@ -23,30 +21,20 @@ export default async function handler(
       });
     }
 
-    // Fallback in case the button is falsely active for some reason
-    const holdsPOAP = await getPOAPStatus(ethAddress);
-    if (!holdsPOAP) {
-      res.status(403).send({
-        message: "User does not have a valid NFT"
-      });
-      return;
-    }
-
     const discountCode = await generateShopifyProductDiscount(ethAddress);
 
     const cartResponse = await createCart(discountCode);
 
     if (!discountCode || !cartResponse) {
-      res.status(400).json({
+      return res.status(400).json({
         message: "Could not create cart"
       });
     }
 
-    res.status(200).json({
+    return res.status(200).json({
       discountCode: discountCode,
       cartResponse: cartResponse
     });
-    return;
   } catch (error) {
     console.error("Error creating cart:", error);
     return res.status(400).json({
